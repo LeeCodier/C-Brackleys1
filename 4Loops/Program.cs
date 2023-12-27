43f53ed901827264471cc6f057ff230cd78b1838
LeeCodier <thatobusm@gmail.com>
2023-12-27 20:44:56 +0200

    Finished for Loops not gonna do while loops

--- 4Loops/Program.cs
+++ 4Loops/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+
+namespace Loops{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         Console.WriteLine("How many cool numbers do you want: ");
+
+         int count = Convert.ToInt32(Console.ReadLine());
+         for (int i = 1; i <= count; i++)
+         {
+               double result = Math.Pow(2, i);
+               Console.WriteLine(result);
+        }
+
+        Console.ReadKey();
+        }
+    }
+}
