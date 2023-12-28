4648c20f2022dc5906ab2a65af17b16715c8a4c9
LeeCodier <thatobusm@gmail.com>
2023-12-28 11:08:26 +0200

    Added Methotds

---
+++ 6Methods/Program.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+
+namespace Methods{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+     //    MeetAlien();
+           Console.WriteLine("PLease input a number to be sqaured");
+           int input = Convert.ToInt32(Console.ReadLine());
+           
+           int returned = Square(input);
+
+           Console.WriteLine(input +  " Squared is: " + returned);
+
+         Console.ReadKey();
+        }
+
+
+
+
+    //method that returns things
+   static int Square(int number)
+    {
+        int result = number * number;
+        return result;
+    }
+      
+
+    //    //Basic method 
+
+    //    static void MeetAlien(){
+    //     Random numberGen = new Random();
+
+    //     string name = "X-" + numberGen.Next(1,100);
+    //     int age = numberGen.Next(10,500);
+
+    //     Console.WriteLine("Hi, I'm " + name);
+    //     Console.WriteLine("I'm " + age + " years old");
+    //    }
+    }
+}
